4d36f0cdb4836a8591bf26183d04610d236f965a
TossoTosca <nugrohosantoso454545@gmail.com>
2022-12-21 12:59:21 +0700

    bin www udh ada port jd gausah kasih port di app.js

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = 3000;
+// const port = 3000;
 const routes = require("./routes/");
 const errorHandler = require("./middlewares/errorHandler");
 
@@ -10,8 +10,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/", routes);
 app.use(errorHandler)
 
-app.listen(port, () => {
-  console.log(`Muyen port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Muyen port ${port}`);
+// });
 
 module.exports = app;
